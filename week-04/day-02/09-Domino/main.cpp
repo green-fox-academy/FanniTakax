9fec44bb0ae9942063e3bba352ebc8433d7ff95f
FanniTakax <tkcsfni@gmail.com>
2018-11-28 14:40:22 +0100

    dominoes_progress

--- week-04/day-02/09-Domino/main.cpp
+++ week-04/day-02/09-Domino/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "domino.h"
+
+std::vector<Domino> initializeDominoes()
+{
+    std::vector<Domino> dominoes;
+    dominoes.push_back(Domino(5, 2));
+    dominoes.push_back(Domino(4, 6));
+    dominoes.push_back(Domino(1, 5));
+    dominoes.push_back(Domino(6, 7));
+    dominoes.push_back(Domino(2, 4));
+    dominoes.push_back(Domino(7, 1));
+    return dominoes;
+}
+
+int main(int argc, char* args[])
+{
+    std::vector<Domino> dominoes = initializeDominoes();
+
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    std::vector<Domino> dominoes_2;
+
+    //(dominoes[j].getValues().second == dominoes[i].getValues().first)
+
+    dominoes_2.push_back(dominoes[1]);
+
+    for (int i = 0; i < dominoes_2.size(); ++i) {
+        for (int j = 0; j < dominoes.size(); ++j) {
+            if(dominoes_2[i].getValues().second == dominoes[j].getValues().first){
+                dominoes_2.push_back(dominoes[j]);
+            }
+        }
+    }
+
+    for (int k = 0; k < dominoes_2.size(); ++k) {
+        std::cout << dominoes_2[k].toString() << ", ";
+    }
+
+
     return 0;
 }
