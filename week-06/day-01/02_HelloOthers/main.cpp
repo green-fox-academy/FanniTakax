f48895ffc359d7e82d058e64b92690d7e7d0d85c
FanniTakax <tkcsfni@gmail.com>
2018-12-05 12:48:10 +0100

    c_workshop_02

--- week-06/day-01/02_HelloOthers/main.cpp
+++ week-06/day-01/02_HelloOthers/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include<stdio.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    char *mate1 = "Judit";
+    char *mate2 = "Johnny";
+    char *mate3 = "Mimi";
+
+    printf("Hello, %s!\n", mate1);
+    printf("Hello, %s!\n", mate2);
+    printf("Hello, %s!\n", mate3);
+
     return 0;
 }
