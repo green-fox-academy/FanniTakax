d3bd1f1885aeab6ae97ee5dbd96603aaf4aa8e01
FanniTakax <tkcsfni@gmail.com>
2018-12-06 16:17:23 +0100

    c_workshop

--- week-06/day-02/02-Separate/main.c
+++ week-06/day-02/02-Separate/main.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    printf("The area of the circle with %.2f cm radius is %.2f cm2", 6.18, circleArea(6.18));
-    printf("The circumference of the circle with %.2f cm radius is %.2f cm", 6.18, circleCircumference(6.18));
+    printf("The area of the circle with %.2f cm radius is %.2f cm2\n", 6.18, circleArea(6.18));
+    printf("The circumference of the circle with %.2f cm radius is %.2f cm\n", 6.18, circleCircumference(6.18));
 
 
     return 0;
