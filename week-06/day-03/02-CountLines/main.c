3043101606cfd2d6017a549c2af9282937ea184b
FanniTakax <tkcsfni@gmail.com>
2018-12-07 13:34:03 +0100

    c_workshop

---
+++ week-06/day-03/02-CountLines/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file
+
+int file_lines(char * file_name)
+{
+    FILE * fptr;
+    int counter = 0;
+    fptr = fopen(file_name, "r");
+    char line[111];
+    while(!feof(fptr)){
+        fgets(line, 111, fptr);
+        counter++;
+    }
+    return counter;
+}
+
+int main ()
+{
+    printf("The text file has %d lines", file_lines("my-file.txt"));
+
+    return 0;
+}
