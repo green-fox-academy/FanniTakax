897d8c13ea8cddb67e817b541f9f1ba5c2ab9430
FanniTakax <tkcsfni@gmail.com>
2018-12-11 12:52:12 +0100

    c_workshop

--- week-07/day-02/02-Calloc/main.c
+++ week-07/day-02/02-Calloc/main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// with malloc
+// with calloc
 // please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
 // please delete the array before the program exits
-
+// what's the difference between this and the previous excersice? >>> 0 istead of "memoryshit"
 
 int main()
 {
     int* pointer = NULL;
 
-    pointer = malloc(10 * sizeof(int));
+    pointer = calloc(10, sizeof(int));
 
     for (int i = 0; i < 5; ++i) {
         pointer[i] = i;
+        //printf(">> %d\n", pointer[i]);
     }
 
     for (int j = 0; j < 10; ++j) {
         printf(">> %d\n", pointer[j]);
     }
-
 
     free(pointer);
 
