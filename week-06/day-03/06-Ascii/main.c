69bcd7468f2950eb11884be5201bbdfbae7a9379
FanniTakax <tkcsfni@gmail.com>
2019-01-03 18:41:54 +0100

    c_practice_correction

--- week-06/day-03/06-Ascii/main.c
+++ week-06/day-03/06-Ascii/main.c
@@ -6,5 +6,11 @@ int main()
 {
     int array[] = { 103, 114, 101, 101, 110, 32, 102, 111, 120};
 
+    int size = sizeof(array) / sizeof(array[0]);
+
+    for(int i = 0; i < size; i++){
+        printf("%c", (char) array[i]);
+    }
+
     return 0;
 }
