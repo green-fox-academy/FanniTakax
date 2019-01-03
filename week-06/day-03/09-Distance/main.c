8cc7ef5952159e213dcd727adf79279f7e54d24f
FanniTakax <tkcsfni@gmail.com>
2019-01-03 22:51:09 +0100

    c_practice_correction

--- week-06/day-03/09-Distance/main.c
+++ week-06/day-03/09-Distance/main.c
@@ -4,20 +4,34 @@
 // create a function which takes a char array as a parameter and
 // returns the distance between the first and last occurance of character 's'
 
+int distance_btw_char(char array[]);
+
 int main ()
 {
     char str[] = "This is a sample string";
-    char *first, *last;
+    //char *first, *last;
+
+    printf("The distance between the first and last occurrance of 's' in the string is >>> %d <<< .", distance_btw_char(str));
 
     return 0;
 }
 
-int distance_btw_char(char charray[], char c)
+int distance_btw_char(char array[])
 {
-    int counter = 0;
-    for(int i = 0; i < strlen(charray); i++){
-        counter++;
-        if(charray[i] == c)
+    char s = 's';
+    int first_counter = 0;
+    for(int i = 0; i < strlen(array); i++){
+        first_counter++;
+        if(array[i] == s)
             break;
     }
+
+    int last_counter = strlen(array);
+    for(int i = strlen(array); i > 0; i--){
+        last_counter--;
+        if(array[i] == s)
+            break;
+    }
+
+    return (last_counter - first_counter);
 }
